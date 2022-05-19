01b8c834cb9eec6073b8d63130bfed6454be2cd4
卜权 <buquan@192.168.0.101>
2022-05-19 18:04:22 +0800

    feat:index

--- pages/about/about.vue
+++ pages/about/about.vue
@@ -1,60 +1,60 @@
 <template>
-	<view>
-
-		<!-- <x v-bind="{value, minDate, maxDate}" v-model="value1"></x> -->
-		<y @input="onInput" v-model="val" v-bind="{value: val2}"></y>
-		<view>outval: -> {{val}}</view>
-<!-- 		<z :info="info">
+  <view>
+    <!-- <x v-bind="{value, minDate, maxDate}" v-model="value1"></x> -->
+    <y @input="onInput" v-model="val" v-bind="{ value: val2 }"></y>
+    <view>outval: -> {{ val }}</view>
+    <!-- 		<z :info="info">
 			<view>{{info.name}}</view>
 		</z> -->
 
-<!--	<view @click="changeZ">
+    <!--	<view @click="changeZ">
 			changeZ info:name{{info.name}}
 		</view> -->
-	</view>
+  </view>
 </template>
 
 <script>
-	import X from './com/x.vue'
-	import Y from './com/y.vue'
-	import Z from './com/z.vue'
-	export default {
-		data() {
-			const now = +new Date();
-			return {
-				val: '888',
-				val2: '999',
-				value1: now,
-				value: 0,
-				minDate: now + 60 * 60 * 1000,
-				maxDate: now + 60 * 24 * 60 * 60 * 1000,
-				info: {}
-			}
-		},
-		computed: {
-			info1() {
-				console.log('info1 computed')
-				return this.name + 'qwert';
-			}
-		},
-		mounted() {},
-		components: {
-			X,
-			Y,
-			Z
-		},
-		methods: {
-			onInput(e) {
-				console.log(e, 'outer');
-				// this.val = e + '666';
-			},
-			changeZ() {
-				this.info.name = 'abc'
-				this.info = {
-					...this.info
-				}
-				// this.$forceUpdate();
-			}
-		}
-	}
+import X from './com/x.vue'
+import Y from './com/y.vue'
+import Z from './com/z.vue'
+export default {
+  data() {
+		const a = 999;
+    const now = +new Date();
+    return {
+      val: '888',
+      val2: '999',
+      value1: now,
+      value: 0,
+      minDate: now + 60 * 60 * 1000,
+      maxDate: now + 60 * 24 * 60 * 60 * 1000,
+      info: {}
+    }
+  },
+  computed: {
+    info1() {
+      console.log('info1 computed')
+      return this.name + 'qwert';
+    }
+  },
+  mounted() { },
+  components: {
+    X,
+    Y,
+    Z
+  },
+  methods: {
+    onInput(e) {
+      console.log(e, 'outer');
+      // this.val = e + '666';
+    },
+    changeZ() {
+      this.info.name = 'abc'
+      this.info = {
+        ...this.info
+      }
+      // this.$forceUpdate();
+    }
+  }
+}
 </script>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,73 +1,73 @@
 <template>
-	<view>
-		<u-button color="red" text="按钮"></u-button>
-		<button type="default" @click="jump">跳转</button>
-		<demo2></demo2>
-	</view>
+  <view>
+    <u-button color="red" text="按钮"></u-button>
+    <button type="default" @click="jump">跳转</button>
+    <demo2></demo2>
+  </view>
 </template>
 
 <script>
-	// 以下是wx mp 打印顺序 （注意：小程序和浏览器端打印顺序不一样！！）
-	// App Launch
-	// App.vue:7 App Show
-	// index.vue:39 index beforeCreate
-	// index.vue:42 index created
-	// index.vue:45 index beforeMount
-	// demo2.vue:19 demo2 beforeCreate
-	// demo2.vue:22 demo2 created
-	// demo2.vue:26 demo2 beforeMount
-	// index.vue:24 index onLoad
-	// index.vue:27 index onShow
-	// demo2.vue:29 demo2 mounted
-	// index.vue:48 index mounted
-	// index.vue:30 index onReady
-	export default {
-		name: "index",
-		data() {
-			return {
+// 以下是wx mp 打印顺序 （注意：小程序和浏览器端打印顺序不一样！！）
+// App Launch
+// App.vue:7 App Show
+// index.vue:39 index beforeCreate
+// index.vue:42 index created
+// index.vue:45 index beforeMount
+// demo2.vue:19 demo2 beforeCreate
+// demo2.vue:22 demo2 created
+// demo2.vue:26 demo2 beforeMount
+// index.vue:24 index onLoad
+// index.vue:27 index onShow
+// demo2.vue:29 demo2 mounted
+// index.vue:48 index mounted
+// index.vue:30 index onReady
+export default {
+  name: "index",
+  data() {
+    return {
 
-			};
-		},
-		methods: {
-			jump() {
-				uni.redirectTo({
-					url: '../../pages/log/log'
-				})
-			}
-		},
-		onLoad() {
-			console.log('index onLoad')
-		},
-		onShow() {
-			console.log('index onShow')
-		},
-		onReady() {
-			console.log('index onReady')
-		},
-		onHide() {
-			console.log('index onHide')
-		},
-		onUnload() {
-			console.log('index onUnload')
-		},
-		beforeCreate() {
-			console.log('index beforeCreate')
-		},
-		created() {
-			console.log('index created')
-		},
-		beforeMount() {
-			console.log('index beforeMount')
-		},
-		mounted() {
-			console.log('index mounted')
-		},
-		beforeDestroy() {
-			console.log('index beforeDestroy')
-		},
-		destroyed() {
-			console.log('index destroyed')
-		},
+    };
+  },
+  methods: {
+    jump() {
+      uni.redirectTo({
+        url: '../../pages/log/log'
+      })
+    }
+  },
+  onLoad() {
+    console.log('index onLoad')
+  },
+  onShow() {
+    console.log('index onShow')
+  },
+  onReady() {
+    console.log('index onReady')
+  },
+  onHide() {
+    console.log('index onHide')
+  },
+  onUnload() {
+    console.log('index onUnload')
+  },
+  beforeCreate() {
+    console.log('index beforeCreate')
+  },
+  created() {
+    console.log('index created')
+  },
+  beforeMount() {
+    console.log('index beforeMount')
+  },
+  mounted() {
+    console.log('index mounted')
+  },
+  beforeDestroy() {
+    console.log('index beforeDestroy')
+  },
+  destroyed() {
+    console.log('index destroyed')
+  },
 
-	}
+}
 </script>
